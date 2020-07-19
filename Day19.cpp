b429fe51b10e73d572c51f45bdfd4cbff63490f4
Mukul Vashishth <67701494+MukulVashishth@users.noreply.github.com>
2020-07-19 23:20:28 +0530

    Adding binary strings

---
+++ Day19.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        if(a == "0")
+            return b;
+        if(b == "0")
+            return a;
+        string res;
+        int i = a.length()-1 , j = b.length()-1;
+        int carry = 0;
+        while(i>=0 || j>=0)
+        {
+            int sum = carry;
+            if(i>=0)
+                sum += a[i--] - '0';
+            if(j>=0)
+                sum += b[j--] - '0';
+            carry = sum>1?1:0;
+            res += to_string(sum%2);
+        }
+        if(carry)
+            res += to_string(carry);
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
