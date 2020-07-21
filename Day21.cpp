b3d5ef57d6114040a100cfc6b7d16f755ca1a415
Mukul Vashishth <67701494+MukulVashishth@users.noreply.github.com>
2020-07-21 20:08:11 +0530

    Word Search

---
+++ Day21.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool dfs(vector<vector<char>>& board , int i, int j, string& word , int idx , vector<vector<bool>>& visited)
+    {
+        if(idx == word.length()-1)
+            return true;
+        visited[i][j] = true;
+        if(i>0 && !visited[i-1][j] && board[i-1][j] == word[idx+1] && dfs(board,i-1,j,word,idx+1,visited))
+            return true;
+        
+        if(j>0 && !visited[i][j-1] && board[i][j-1] == word[idx+1] && dfs(board,i,j-1,word,idx+1,visited))
+            return true;
+ 
+        if(i<board.size()-1 && !visited[i+1][j] && board[i+1][j] == word[idx+1] && dfs(board,i+1,j,word,idx+1,visited))
+            return true;
+        
+    if(j<board[0].size()-1 && !visited[i][j+1] && board[i][j+1] == word[idx+1] && dfs(board,i,j+1,word,idx+1,visited))
+            return true;
+        
+        visited[i][j] = false;
+        return false;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+        
+        vector<vector<bool>> visited(rows,vector<bool>(cols , false));
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(board[i][j] == word[0] && dfs(board,i,j,word,0,visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
